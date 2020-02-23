d01647222cbb2642270fbd330a6561bdd9a49ba4
DSobkov <danylo.sobkov@ukr.net>
2020-02-23 15:35:34 +0200

    +-

--- main.go
+++ main.go
@@ -8,10 +8,9 @@ import (
 )
 
 func main() {
-	fmt.Println("Listening on port :9949")
-	http.ListenAndServe(":9949", nil)
+	fmt.Println("Listening on port :9999")
+	http.ListenAndServe(":9999", nil)
 	http.HandleFunc("/", mainPage)
-	http.HandleFunc("/users", usersPage)
 }
 
 type User struct {
@@ -21,8 +20,8 @@ type User struct {
 }
 
 func mainPage(w http.ResponseWriter, r *http.Request)  {
-	// user := User{"Danylo", "Sobkov"}
-	// js, _ := json.Marshal(user)
+	// user := User{"Danylo"}
+	//	// js, _ := json.Marshal(user)
 
 	tmpl, err := template.ParseFiles("index.html")
 	if err != nil {
@@ -36,18 +35,3 @@ func mainPage(w http.ResponseWriter, r *http.Request)  {
 	}
 }
 
-func usersPage(w http.ResponseWriter, r *http.Request)  {
-	users := []User{User{"Danylo", "Sobkov"}, User{"Vlad", "Orlov"}}
-	// js, _ := json.Marshal(user)
-
-	tmpl, err := template.ParseFiles("users.html")
-	if err != nil {
-		http.Error(w, err.Error(), 400)
-		return
-	}
-
-	if err := tmpl.Execute(w, users); err !=nil {
-		http.Error(w, err.Error(), 400)
-		return
-	}
-}
